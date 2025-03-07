e29ebb1a4f5e931133ee4926f50d939314d3fe82
anndra17 <andra.horhocea@student.unitbv.ro>
2025-03-07 12:39:50 +0200

    Creating the application base

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -4,10 +4,8 @@ import { Text, View, StyleSheet } from 'react-native';
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="./profile" style={styles.button}>
-        Go to Profile screen
-      </Link>
+      <Text style={styles.text}>Profile screen</Text>
+      
     </View>
   );
 }
