660207ad1f86d56a4811c730c7d62311c7fca03d
anndra17 <andra.horhocea@student.unitbv.ro>
2025-05-21 17:49:57 +0300

    refactor: Simplify LoginScreen to redirect to sign-in page

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -1,30 +1,5 @@
-import Uuid from "expo-modules-core/src/uuid";
-import {ReactNode} from "react";
-import {Button, Text, View, StyleSheet} from "react-native";
-import 'react-native-gesture-handler';
-import { Colors} from "../../constants/Colors";
+import { Redirect } from 'expo-router';
 
-
-
-export default function LoginScreen() {
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Login Screen</Text>
-      <Button title="Sign in with Google"/>
-    </View>
-  );
+export default function Index() {
+  return <Redirect href="/login/sign-in" />;
 }
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: Colors.light.background,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    text: {
-      fontSize: 20, // Increased font size
-      marginBottom: 20, // Added margin
-    },
-  });
